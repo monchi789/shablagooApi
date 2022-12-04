de3c87a5b4ff2d2933595c7fd79ad66a571976d1
Cristian <cristian-02-@live.com>
2022-12-04 16:37:27 -0500

    Fix the routes

--- api/urls.py
+++ api/urls.py
@@ -8,11 +8,11 @@ from .api import *
 
 router = routers.DefaultRouter()
 
-router.register('api/roles', RoleViewSet, 'roles')
-router.register('api/users', UserViewSet, 'users')
-router.register('api/eventPlanners', EventPlannerViewSet, 'eventPlanners')
-router.register('api/categorys', CategoryViewSet, 'categorys')
-router.register('api/events', EventViewSet, 'events')
+router.register('/roles', RoleViewSet, 'roles')
+router.register('/users', UserViewSet, 'users')
+router.register('/eventPlanners', EventPlannerViewSet, 'eventPlanners')
+router.register('/categorys', CategoryViewSet, 'categorys')
+router.register('/events', EventViewSet, 'events')
 
 urlpatterns = [
     re_path(r'^media/(?P<path>.*)$', serve, {
